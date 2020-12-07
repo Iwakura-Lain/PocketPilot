42a6c835f765f1179de5dba4b666a011b7bc7cba
Aleksandar <aleksandar.malinov@ue-germany.de>
2020-12-07 23:08:18 +0000

    Added a simple Minimap in "TestAleks" ( don't touch! ) Added 2 new prefabs made for the minimap

---
+++ Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform player; //camera following position (Transform) of player (player linked to actual player)
+    public Vector3 offset; //Vector3 stores 3 float numbers
+    void Update()
+    {   //position of camera equal to position of player
+        transform.position = player.position + offset;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
+

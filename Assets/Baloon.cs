6ffb6ce46e8678759bf9ecc7f0a4e92759b90e48
Maria <maria.shchurova@ue-germany.de>
2021-01-02 15:01:44 +0100

    baloon level

---
+++ Assets/Baloon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baloon : MonoBehaviour
+{
+    public float floatStrength = 3.5f; 
+         
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * floatStrength);
+    }
+}

5af95644bc1bcfb976d7b26f1e985e3166df578a
Aleksandar <aleksandar.malinov@ue-germany.de>
2021-01-14 22:12:20 +0000

    fixed rotation issues on the minimap

--- Assets/Scripts/ArowFollow.cs
+++ Assets/Scripts/ArowFollow.cs
@@ -8,9 +8,12 @@ public class ArowFollow : MonoBehaviour
 {
     public Transform ObjectToDisplay;
 
+
     void Update()
     {
         //position of camera equal to position of player
         transform.position = ObjectToDisplay.position;
+
+        transform.rotation = Quaternion.Euler(90f, ObjectToDisplay.eulerAngles.y, 0f);
     }
 }

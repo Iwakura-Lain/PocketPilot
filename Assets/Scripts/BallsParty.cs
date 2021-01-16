49637badf0629cfc40254959278e4d0023f5e753
Aleksandar <aleksandar.malinov@ue-germany.de>
2021-01-16 14:15:12 +0000

    update BirthdayParty level

--- Assets/Scripts/BallsParty.cs
+++ Assets/Scripts/BallsParty.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BallsParty : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-     
-    }
-
+    public void TurnColliderOn()
+        {
+            GetComponent<CapsuleCollider>().enabled = true;
+        }
+    
     // Update is called once per frame
     void Update()
     {
         
-    }
+    } 
+            
+    private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+                {
+                    TurnColliderOn();
+                }
+        }
 }
